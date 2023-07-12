f5b27968bfa35562cd56d6f6072f91c54a30963d
Dmitri Dmitriev <d.dzmitryeu@gmail.com>
2023-07-12 16:32:31 +0300

    update

--- Tidy Tags & Spaces/src/getMarkerShift.ts
+++
@@ -1,6 +0,0 @@
-//@ts-nocheck
-export default function getMarkerShift(marker: InstanceNode) {
-  const markerText = marker.findOne((node) => node.type === "TEXT");
-  const difference = 16 - markerText.width;
-  return 56 - difference;
-}
